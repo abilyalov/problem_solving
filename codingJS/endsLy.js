6bbb0683982271dc2857eed326f051fb94cbde9f
Anuar <a_bilyalov@mail.ru>
2020-03-29 02:48:01 +0600

    29.03
    Solved 16 tasks from String-1 , and another one task from DataStructure;

---
+++ codingJS/endsLy.js
@@ -0,0 +1,6 @@
+function endsLy(str){
+  if (str.substring(str.length-2) === 'ly') {
+    return true;
+  } else {
+    return false;
+}
---
+++ codingJS/extraEnd.js
@@ -0,0 +1,3 @@
+function extraEnd(str){
+  return str.slice(-2) + str.slice(-2) + str.slice(-2);
+}
